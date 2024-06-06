a8ed954d4b09d5a43155ba45af8b3c3d3489b8d5
Renanmdr <renanmdr3@gmail.com>
2024-06-06 13:33:51 -0300

    feat: added prisma schema

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 
 export const app = fastify();
+
+prisma.user.create({
+  data: {
+    name: 'Lucas',
+    email: 'Lucas@gmail.com',
+    
+  }
+});
