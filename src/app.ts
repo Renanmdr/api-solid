03882c9ac6c6fd327a4160af81e1aacd42f24283
Renanmdr <renanmdr3@gmail.com>
2024-06-08 21:41:50 -0300

    feat: added routes and register controller

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,8 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { appRoutes } from './http/routes';
 
 
 export const app = fastify();
 
-prisma.user.create({
-  data: {
-    name: 'Lucas',
-    email: 'Lucas@gmail.com',
-    
-  }
-});
+app.register(appRoutes);
+
